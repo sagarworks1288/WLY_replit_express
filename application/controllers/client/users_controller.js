ebde5cf04a2d1d37b3debc1aa2b94ae0dff64b2c
sagarworks1288 <sagarworks1288@gmail.com>
2023-08-31 16:57:19 +0530

    user list added

---
+++ application/controllers/client/users_controller.js
@@ -0,0 +1,35 @@
+const _ = require("lodash");
+const { OAuth2Client } = require("google-auth-library");
+const UsersModel = require("../../models/users_model");
+const AuthHelper = require("../../helpers/auth_helper");
+const Config = require("../../../config");
+
+
+
+async function list(req, res) {
+    try {
+        const payload = _.get(req, "body", {});
+        const perPage = 5
+        const page = _.get(payload, 'page', 1)
+        const skipPage = page > 1 ? ((page - 1) * perPage) : 0
+        const data = await UsersModel.aggregate([
+            // { $match: query },
+          //  { $sort: sortObj },
+            {
+              $facet: {
+                totalData: [{ $skip: skipPage }, { $limit: perPage }],
+                totalCount: [{ $count: 'count' }]
+              }
+            }
+          ])
+        return res.json({
+            code: 1,
+            data: data,
+        });
+    } catch (error) {
+        return res.json({ code: 0, error: error.message });
+    }
+}
+
+
+module.exports = { list };
--- application/routes/api.js
+++ application/routes/api.js
@@ -3,6 +3,7 @@ let router = express.Router();
 
 const ClientHomeController = require("../controllers/client/home_controller");
 const ClientAuthController = require("../controllers/client/auth_controller");
+const ClientUsersController = require("../controllers/client/users_controller");
 const AuthMiddleware = require("../middleware/auth");
 
 /*----------------*/
@@ -10,5 +11,8 @@ router.get( "/", ClientHomeController.index);
 router.get( "/test", ClientHomeController.test);
 router.post( "/client/sign-up", ClientAuthController.signUp);
 router.post( "/client/auth",AuthMiddleware, ClientAuthController.auth);
+router.post( "/client/users",AuthMiddleware, ClientUsersController.list);
+
+
 
 module.exports = router;
